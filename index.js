c85a52f10e009a4e8a7a67ce776df48fc89af2f7
Lui de la Parra <Lui@mutesymphony.com>
2019-04-15 14:32:58 +0800

    Boom Object Refactor
    This commit refactors the traversal of the boom object to use Object’s getOwnPropertyNames method.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ module.exports = () => (req, res, next) => {
   if (res.boom) throw new Error('boom already exists on response object');
 
   res.boom = {};
-  Object.keys(boom).forEach(function(key) {
+  Object.getOwnPropertyNames(boom).forEach(function(key) {
     if (typeof boom[key] !== 'function') return;
     if (helperMethods.indexOf(key) !== -1) {
-      res.boom[key] = function() {
-        return boom[key].apply(boom, arguments);
-      };
+      res.boom[key] = () => boom[key].apply(boom, arguments);
     } else {
       res.boom[key] = function() {
         const boomed = boom[key].apply(boom, arguments);
-
         const boomedPayloadAndAditionalResponse = Object.assign(
           boomed.output.payload,
           arguments[1]
